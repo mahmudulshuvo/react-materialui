3d028b6c2a48f4eb0352cfffb98be2469806a197
ShuvoHassan <shuvo0201@gmail.com>
2018-10-09 22:16:10 +0600

    paging with sequential fetching

--- src/components/data-grid/DataGrid.js
+++ src/components/data-grid/DataGrid.js
@@ -1,65 +1,130 @@
-import React, { Component } from "react";
+import * as React from "react";
 import Paper from "@material-ui/core/Paper";
-import { PagingState, IntegratedPaging } from "@devexpress/dx-react-grid";
+import { PagingState, CustomPaging } from "@devexpress/dx-react-grid";
 import {
     Grid,
     Table,
     TableHeaderRow,
     PagingPanel
 } from "@devexpress/dx-react-grid-material-ui";
-import "./DataGrid.css";
 
-class DataGrid extends Component {
+import Loading from "./CircularIndeterminate";
+
+const URL = "https://js.devexpress.com/Demos/WidgetsGallery/data/orderItems";
+
+export default class DataGrid extends React.PureComponent {
     constructor(props) {
         super(props);
-        console.log("constructor called");
+
         this.state = {
             columns: [
-                { name: "userId", title: "userId" },
-                { name: "id", title: "id" },
-                { name: "title", title: "title" },
-                { name: "body", title: "body" }
+                { name: "OrderNumber", title: "Order Number" },
+                { name: "OrderDate", title: "Order Date" },
+                { name: "StoreCity", title: "Store City" },
+                { name: "StoreState", title: "Store State" },
+                { name: "Employee", title: "Employee" },
+                { name: "SaleAmount", title: "Sale Amount" }
             ],
             rows: [],
-            pageSizes: [5, 10, 15, 0]
+            totalCount: 0,
+            pageSize: 6,
+            currentPage: 0,
+            loading: true,
+            isFirst: false
         };
+
+        this.changeCurrentPage = this.changeCurrentPage.bind(this);
     }
 
     componentDidMount() {
-        fetch("https://jsonplaceholder.typicode.com/posts/")
-            .then(response => {
-                return response.json();
-            })
+        this.loadData();
+    }
+
+    componentDidUpdate() {
+        this.loadData();
+    }
+
+    changeCurrentPage(currentPage) {
+        this.setState({
+            loading: true,
+            isFirst: true,
+            currentPage
+        });
+    }
+
+    queryString() {
+        const { pageSize, currentPage } = this.state;
+
+        return `${URL}?take=${pageSize}&skip=${pageSize * currentPage}`;
+    }
+
+    loadData() {
+        const queryString = this.queryString();
+        if (queryString === this.lastQuery) {
+            this.setState({
+                loading: false,
+                isFirst: false
+            });
+            return;
+        }
+
+        fetch(queryString)
+            .then(response => response.json())
             .then(data => {
-                console.log(data);
+                console.log(data.items);
+                console.log(data.totalCount);
                 this.setState({
-                    rows: data
+                    rows: data.items,
+                    totalCount: data.totalCount,
+                    loading: false
                 });
             })
-            .catch(err => {
-                console.log(err);
-            });
+            .catch(() => this.setState({ loading: false }));
+        this.lastQuery = queryString;
     }
 
     render() {
-        const { rows, columns, pageSizes } = this.state;
+        const {
+            rows,
+            columns,
+            pageSize,
+            currentPage,
+            totalCount,
+            loading,
+            isFirst
+        } = this.state;
+
         return (
             <div className="data-grid-div">
-                <Paper style={{ overflowY: "auto" }}>
-                    <Grid rows={rows} columns={columns}>
-                        <PagingState
-                            defaultCurrentPage={0}
-                            defaultPageSize={5}
-                        />
-                        <IntegratedPaging />
-                        <Table />
-                        <TableHeaderRow />
-                        <PagingPanel pageSizes={pageSizes} />
-                    </Grid>
-                </Paper>
+                <div>
+                    <Paper>
+                        <Grid rows={rows} columns={columns}>
+                            <PagingState
+                                currentPage={currentPage}
+                                onCurrentPageChange={this.changeCurrentPage}
+                                pageSize={pageSize}
+                            />
+                            <CustomPaging totalCount={totalCount} />
+                            <Table />
+                            <TableHeaderRow />
+                            <PagingPanel />
+                        </Grid>
+                        {/* {loading && <Loading  />} */}
+                    </Paper>
+                </div>
+
+                <div
+                    style={{
+                        position: "fixed",
+                        marginLeft: "48%",
+                        marginRight: "48%",
+                        marginTop: "-15%",
+                        zIndex: "1000"
+                    }}
+                >
+                    {loading && isFirst && <Loading />}
+                </div>
             </div>
         );
     }
 }
-
-export default DataGrid;
