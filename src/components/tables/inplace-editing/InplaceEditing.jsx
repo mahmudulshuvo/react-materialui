ca44edfbd1012c8f893a19391eddb994bd29299c
mahmudulshuvo <shuvo0201@gmail.com>
2018-11-23 15:49:02 +0600

    Date picker & Select from Dropdown added

--- src/components/tables/inplace-editing/InplaceEditing.jsx
+++ src/components/tables/inplace-editing/InplaceEditing.jsx
@@ -18,10 +18,35 @@ import IconButton from "@material-ui/core/IconButton";
 import EditIcon from "@material-ui/icons/Edit";
 import SaveIcon from "@material-ui/icons/Save";
 import CancelIcon from "@material-ui/icons/Cancel";
+import Input from "@material-ui/core/Input";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import TableCell from "@material-ui/core/TableCell";
+import { withStyles } from "@material-ui/core/styles";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const getRowId = row => row.id;
 const URL = "https://jsonplaceholder.typicode.com/posts";
 let defaultColumnValues = {};
+let placedDate = new Date();
+let dateValue = "2018-11-23";
+let dateChangeFlag = false;
+
+const styles = theme => ({
+  lookupEditCell: {
+    paddingTop: theme.spacing.unit * 0.875,
+    paddingRight: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit
+  },
+  dialog: {
+    width: "calc(100% - 16px)"
+  },
+  inputRoot: {
+    width: "100%"
+  }
+});
 
 const EditButton = ({ onExecute }) => (
   <IconButton onClick={onExecute} title="Edit row">
@@ -59,15 +84,17 @@ export function generateRows({
 }) {
   const data = [];
   const columns = Object.keys(columnValues);
+  //console.log("columns", columns);
 
   for (let i = 0; i < length; i += 1) {
     const record = {};
 
     columns.forEach(column => {
       let values = columnValues[column];
+      //console.log("values", values);
       if (typeof values === "function") {
         record[column] = values({ random, index: i, record });
-        console.log("In function");
+        //console.log("In function");
         return;
       }
 
@@ -88,7 +115,66 @@ export function generateRows({
   return data;
 }
 
+const availableValues = {
+  product: ["Audi", "BMW", "Toyota", "Hundai", "Nissan"]
+};
+
+const LookupEditCellBase = ({
+  availableColumnValues,
+  value,
+  onValueChange,
+  classes
+}) => (
+  <TableCell className={classes.lookupEditCell}>
+    <Select
+      value={value}
+      onChange={event => onValueChange(event.target.value)}
+      input={<Input classes={{ root: classes.inputRoot }} />}
+    >
+      {availableColumnValues.map(item => (
+        <MenuItem key={item} value={item}>
+          {item}
+        </MenuItem>
+      ))}
+    </Select>
+  </TableCell>
+);
+export const LookupEditCell = withStyles(styles, {
+  name: "ControlledModeDemo"
+})(LookupEditCellBase);
+
+const EditCell = props => {
+  const { column } = props;
+  const availableColumnValues = availableValues[column.name];
+  if (availableColumnValues) {
+    return (
+      <LookupEditCell
+        {...props}
+        availableColumnValues={availableColumnValues}
+      />
+    );
+  }
+  if (column.name === "date") {
+    return (
+      <TableCell>
+        <DatePicker selected={placedDate} onChange={handleChange} />
+      </TableCell>
+    );
+  }
+
+  return <TableEditRow.Cell {...props} />;
+};
+
+function handleChange(date) {
+  // console.log("function called", date);
+  placedDate = date;
+  console.log("date value ", placedDate);
+  dateValue = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
+  console.log("date year", dateValue);
+  dateChangeFlag = true;
+}
 class InplaceEditing extends React.PureComponent {
+  //This is almost clean with a minor issue
   constructor(props) {
     super(props);
 
@@ -99,20 +185,49 @@ class InplaceEditing extends React.PureComponent {
           id: ({ index }) => index,
           ...defaultColumnValues
         }
-      })
+      }),
+      date: dateValue
     };
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSelect = this.handleSelect.bind(this);
+    console.log("defaultColumnValues", defaultColumnValues);
   }
 
   componentDidMount() {
     this.fetchData();
   }
 
+  handleChange(date) {
+    console.log("handle change called", date);
+    // this.setState({
+    //   date: date
+    // });
+    // console.log("Date property", this.state.date);
+  }
+
+  handleSelect(date) {
+    console.log("handle select called", date);
+    // this.setState({
+    //   date: date
+    // });
+    // console.log("Date property", this.state.date);
+  }
+
   commitChanges = ({ changed }) => {
     let { rows } = this.state;
+    if (dateChangeFlag) {
+      rows = rows.map(row => {
+        if (row.id === parseInt(Object.keys(changed)[0], 10)) {
+          row.date = dateValue;
+        }
+        return row;
+      });
+      dateChangeFlag = false;
+    }
 
     if (changed) {
-      rows = rows.map(
-        row => (changed[row.id] ? { ...row, ...changed[row.id] } : row)
+      rows = rows.map(row =>
+        changed[row.id] ? { ...row, ...changed[row.id] } : row
       );
     }
     this.setState({ rows });
@@ -130,15 +245,27 @@ class InplaceEditing extends React.PureComponent {
             for (var key in obj) {
               let obj = { name: key, title: key };
               defaultColumnValues[key] = [];
+              defaultColumnValues["product"] = [];
+              defaultColumnValues["date"] = [];
               columnArray.push(obj);
             }
+            let newObj = { name: "product", title: "product" };
+            columnArray.push(newObj);
+            let dateObj = { name: "date", title: "date" };
+            columnArray.push(dateObj);
 
             for (var i = 0; i < data.length; i++) {
               //console.log(
               for (var j = 0; j < columnArray.length; j++) {
-                defaultColumnValues[columnArray[j].title].push(
-                  data[i][columnArray[j].title]
-                );
+                if (columnArray[j].title === "product") {
+                  defaultColumnValues["product"].push("Audi");
+                } else if (columnArray[j].title === "date") {
+                  defaultColumnValues["date"].push(this.state.date);
+                } else {
+                  defaultColumnValues[columnArray[j].title].push(
+                    data[i][columnArray[j].title]
+                  );
+                }
               }
             }
           }
@@ -154,7 +281,7 @@ class InplaceEditing extends React.PureComponent {
             })
           });
         } else {
-          console.log("Its not a json array");
+          console.log("It's not a json array");
         }
       })
       .catch(() => console.log("Error in fetching"));
@@ -171,7 +298,7 @@ class InplaceEditing extends React.PureComponent {
             <EditingState onCommitChanges={this.commitChanges} />
             <Table />
             <TableHeaderRow />
-            <TableEditRow />
+            <TableEditRow cellComponent={EditCell} />
             <TableEditColumn showEditCommand commandComponent={Command} />
             <PagingPanel />
           </Grid>
